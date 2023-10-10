61722aba9845de0bd956e796e3c9439c13225d3c
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-10-10 22:52:53 +0800

    位运算、算法、简单排序

---
+++ Class01/src/class01/Code01_SumOfFactorial.java
@@ -0,0 +1,8 @@
+package class01;
+
+/**
+ * @author Xiaojin
+ * @date 2023/10/10
+ */
+public class Code01_SumOfFactorial {
+}
---
+++ Class01/src/class01/Code06_PrintBit.java
@@ -0,0 +1,24 @@
+package class01;
+
+/**
+ * @author Xiaojin
+ * @date 2023/10/10
+ */
+public class Code06_PrintBit {
+
+    public static void print(int num){
+        for(int i=31;i>=0;i--){
+            System.out.print((num & (1<<i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        //32位
+        int num =83928328;
+
+        print(num);
+
+    }
+
+}
