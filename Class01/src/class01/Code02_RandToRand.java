ac27ad13c6040f35f1195a22c6ed2d3d1914e783
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-11-16 23:07:30 +0800

    将任何随机改造成0-1发生器

--- Class01/src/class01/Code02_RandToRand.java
+++ Class01/src/class01/Code02_RandToRand.java
@@ -29,6 +29,29 @@ public class Code02_RandToRand {
             }
         }
         System.out.println((double)count / (double)testTime);
+
+
     }
 
+    //返回[0,1)的一个小数
+    // 任意的x, x属于[0,1), [0,x)范围上的数出现概率为 由原来的x调整成x^2
+    public static double xPower2(){
+        return Math.max(Math.random(),Math.random());
+        //return Math.min(Math.random(),Math.random());  // 这是取 1 - (1-x)^2
+    }
+
+    // lib里的 不能改
+    public static int f1(){
+        return (int) (Math.random() * 5) + 1;
+    }
+
+    //随机机制，只能用f1
+    //等概率返回0和1
+    public static int f2(){
+        int ans = 0;
+        do{
+            ans = f1();
+        } while(ans == 3);
+        return ans < 3 ? 0 : 1;
+    }
 }
