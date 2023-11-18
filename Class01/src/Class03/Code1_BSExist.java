582bdb972b10c380e25fe24f525af958b9c44723
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-11-18 21:17:24 +0800

    求大于等于/小于等于num的最左/最右位置

--- Class01/src/Class03/Code1_BSExist.java
+++ Class01/src/Class03/Code1_BSExist.java
@@ -69,4 +69,22 @@ public class Code1_BSExist {
         return false;
     }
 
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+        while(left <= right){
+            int mid = left + ((right-left) >> 1);
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] > target){
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
 }
