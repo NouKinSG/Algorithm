2a33d576dcee0e0e0177b69f8f5d8426a81237bf
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-11-18 20:49:08 +0800

    二分查找法

---
+++ Class01/src/Class03/Code1_BSExist.java
@@ -0,0 +1,72 @@
+package Class03;
+
+/**
+ * @author Xiaojin
+ * @date 2023/11/17
+ */
+public class Code1_BSExist {
+
+
+    //arr有序，求 >= num的最左位置
+    public static int mostLeftNoLessNumIndex(int[] arr,int num){
+        if(arr == null || arr.length == 0){
+            return -1;
+        }
+        int L = 0;
+        int R = arr.length-1;
+        int ans = -1;
+        while(L <= R){
+            int mid = L + ((R-L) >> 1);
+            if(arr[mid] >= num){
+                ans = mid;
+                R = mid - 1;
+            } else{  // arr[mid] < num
+                L = mid + 1;
+            }
+        }
+        return ans;  // 如果遍历完，都没有找到>=num的数(一直没有进入if 都是else)，返回-1
+    }
+
+    //arr有序，求 <= num的最右位置
+    public static int mostRightNoMoreNumIndex(int[] arr,int num){
+        if(arr == null || arr.length == 0){
+            return -1;
+        }
+        int L = 0;
+        int R = arr.length-1;
+        int ans = -1;
+        while (L <= R) {
+            int mid = L + ((R-L) >> 1);
+            if (arr[mid] <= num) {
+                ans = mid;
+                L = mid + 1;
+            }else{
+                R = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+
+    // 二分查找法
+    public static  boolean find(int[] arr,int num){
+        if(arr ==null || arr.length == 0){
+            return false;
+        }
+
+        int L= 0;
+        int R = arr.length-1;
+        while (L <= R){
+            int mid = L + ((R-L) >> 1);
+            if(arr[mid] == num){
+                return true;
+            } else if (arr[mid] < num) {
+                L = mid + 1;
+            } else{
+                R = mid - 1;
+            }
+        }
+        return false;
+    }
+
+}
