83035d254b3b4638b2ab38c527e93c2c3feec6dc
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-11-30 20:21:37 +0800

    DoubleToDeque

--- Class01/src/Class04/Code01_ReverseList.java
+++ Class01/src/Class04/Code01_ReverseList.java
@@ -38,18 +38,84 @@ public class Code01_ReverseList {
         return pre;
     }
 
-    public static DoubleNode reverseList(DoubleNode head){
+   public static DoubleNode reverseList(DoubleNode head){
         DoubleNode pre = null;
         DoubleNode next = null;
         while(head != null){
-            next = head.next; // 保存下一个节点
-            head.next = pre;  // 当前节点指向前一个节点
-            head.last = next; // 当前节点指向后一个节点
-            pre = head;       // pre指向当前节点
-            head = next;      // head指向下一个节点
+            next = head.next;
+            head.next = pre;
+            head.last = next;
+            pre = head;
+            head = next;
         }
         return pre;
+   }
+
+    public static void printLinkedList(Node head){
+          while(head != null){
+                System.out.print(head.value + " ");
+                head = head.next;
+          }
+         System.out.println();
     }
+
+    public static void printDoubleLinkedList(DoubleNode head){
+        System.out.print("head -> ");
+        DoubleNode end = null;
+        while(head != null){
+            System.out.print(head.value + " ");
+            end = head;
+            head = head.next;
+        }
+        System.out.print("<- tail");
+        System.out.println();
+        while(end != null){
+            System.out.print(end.value + " ");
+            end = end.last;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Node head = null;
+        head = reverseList(head);
+        printLinkedList(head);
+
+        head = new Node(1);
+        head = reverseList(head);
+        printLinkedList(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head = reverseList(head);
+        printLinkedList(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head = reverseList(head);
+        printLinkedList(head);
+
+        DoubleNode dhead = null;
+        dhead = reverseList(dhead);
+        printDoubleLinkedList(dhead);
+
+        dhead = new DoubleNode(1);
+        dhead = reverseList(dhead);
+        printDoubleLinkedList(dhead);
+
+        dhead = new DoubleNode(1);
+        dhead.next = new DoubleNode(2);
+        dhead = reverseList(dhead);
+        printDoubleLinkedList(dhead);
+
+        dhead = new DoubleNode(1);
+        dhead.next = new DoubleNode(2);
+        dhead.next.next = new DoubleNode(3);
+        dhead = reverseList(dhead);
+        printDoubleLinkedList(dhead);
+    }
+
 
     
 
--- Class01/src/Class04/Code03_DoubleLinkedListToDeque.java
+++ Class01/src/Class04/Code03_DoubleLinkedListToDeque.java
@@ -6,4 +6,72 @@ package Class04;
  */
 public class Code03_DoubleLinkedListToDeque {
 
+     public static class Node<V>{
+         public V value;
+         public Node<V> last;
+         public Node<V> next;
+         public Node(V v){
+             this.value = v;
+             last = null;
+             next = null;
+         }
+     }
+
+     public static class MyDeque<V>{
+         private Node<V> head;
+         private Node<V> tail;
+         private int size;
+         public MyDeque(){
+             head = null;
+             tail = null;
+             size = 0;
+         }
+         public boolean isEmpty(){
+             return size == 0;
+         }
+         public int size(){
+             return size;
+         }
+         public void pushHead(V value){
+             Node<V> cur = new Node<>(value);
+             if(head == null){
+                 tail = cur;
+             }else{
+                 cur.next = head;
+                 head.last = cur;
+             }
+             head = cur;
+             size++;
+         }
+
+         public void pushTail(V value){
+             Node<V> cur = new Node<>(value);
+             if(head == null){
+                 head = cur;
+             }else{
+                 cur.last = tail;
+                 tail.next = cur;
+             }
+             tail = cur;
+             size++;
+         }
+
+         public V pollHead(){
+             V ans = null;
+             if(head != null){
+                 ans = head.value;
+                 if(head == tail){
+                     head = null;
+                     tail = null;
+                 }else{
+                     head = head.next;
+                     head.last = null;
+                 }
+                 size--;
+             }
+            return ans;
+        }
+
+     }
+
 }
