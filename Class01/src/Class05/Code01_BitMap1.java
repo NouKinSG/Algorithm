225379817b98ec45e587cc8429e51b6dc16bdc31
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-12-04 22:54:51 +0800

    添加测试对数器

--- Class01/src/Class05/Code01_BitMap1.java
+++ Class01/src/Class05/Code01_BitMap1.java
@@ -1,5 +1,7 @@
 package Class05;
 
+import java.util.HashSet;
+
 /**
  * @author Xiaojin
  * @date 2023/12/3
@@ -27,5 +29,34 @@ public class Code01_BitMap1 {
 
     }
 
+    public static void main(String[] args) {
+        System.out.println("测试开始！");
+        int max = 10000;
+        BitMap bitMap = new BitMap(max);
+        HashSet<Integer> set = new HashSet<>();
+        int testTime = 10000000;
+        for (int i = 0; i < testTime; i++) {
+            int num = (int) (Math.random() * (max + 1));
+            double decide = Math.random();
+            if (decide < 0.333) {
+                bitMap.add(num);
+                set.add(num);
+            } else if (decide < 0.666) {
+                bitMap.delete(num);
+                set.remove(num);
+            } else {
+                if (bitMap.contains(num) != set.contains(num)) {
+                    System.out.println("Oops!");
+                    break;
+                }
+            }
+        }
+        for (int num = 0; num <= max; num++) {
+            if (bitMap.contains(num) != set.contains(num)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("测试结束！");
+    }
 
 }
