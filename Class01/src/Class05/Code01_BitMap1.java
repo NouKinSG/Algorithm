7e145bd044396084dfedf77f7c952477e63f407f
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-12-04 22:53:56 +0800

    一个错误的类

--- Class01/src/Class05/Code01_BitMap1.java
+++ Class01/src/Class05/Code01_BitMap1.java
@@ -6,6 +6,26 @@ package Class05;
  */
 public class Code01_BitMap1 {
 
+    public static class BitMap{
+        private long[] bits;
+
+        public BitMap(int max){
+            bits = new long[(max + 64) >> 6];
+        }
+
+        public void add(int num){
+            bits[num >> 6 ] |= (1 << (num & 63));
+        }
+
+        public void delete(int num){
+            bits[num >> 6] &= ~(1 << (num & 63));
+        }
+
+        public boolean contains(int num){
+            return (bits[num >> 6] & (1 << (num & 63))) != 0;
+        }
+
+    }
 
 
 }
