9c4a62bd5a03d8395728fac4fa415f0e2dc21873
NouKinSG <xiaojin@stud.tjut.edu.cn>
2023-12-06 11:39:17 +0800

    BitMinus

--- Class01/src/Class05/Code03_BitAddMinusMultiDiv.java
+++ Class01/src/Class05/Code03_BitAddMinusMultiDiv.java
@@ -9,11 +9,20 @@ public class Code03_BitAddMinusMultiDiv {
     public static int add(int a,int b){
         int sum = a;
         while( b != 0 ){
-             sum = a ^ b; // 无进位
-             b = (a & b) << 1;
-             a = sum;
+             sum = a ^ b; // 无进位相加信息 ->  sum
+             b = (a & b) << 1;  // 进位信息 ->  b-> b`(进位信息)
+             a = sum;   // a -> a` 无进位相加信息
         }
         return sum;
     }
+
+    public static int negNum(int n){
+        return add(~n,1);
+    }
+
+    public static int minus(int a,int b){
+        return add(a,negNum(b));  //减去一个数，等于加数这个数的负数
+    }
+
 
 }
